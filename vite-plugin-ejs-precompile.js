67c7378adf1ed02c361f687ace10dbc97ed0134f
Carlson Santana Cruz <carlsonsantana@gmail.com>
2026-04-21 22:33:50 -0300

    Minify the template content before generate the template function

--- vite-plugin-ejs-precompile.js
+++ vite-plugin-ejs-precompile.js
@@ -1,16 +1,40 @@
 import lodash from 'lodash';
+import minifyXML from "minify-xml";
 const { template: lodashTemplate } = lodash;
 
 const HTML_ESCAPE_FN = `function escapeFallback(s){var m={'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'};return s==null?'':String(s).replace(/[&<>"']/g,function(c){return m[c];});}`;
+const XML_PROLOG_REGEX = /(<\?xml\s*version="1.0"\s*encoding="UTF-8"\s*\?>)\s*/;
+const XHTML_DOCTYPE_REGEX = /(<!DOCTYPE\s*html\s*PUBLIC\s*"-\/\/W3C\/\/DTD\s*XHTML\s*1\.1\/\/EN"\s*"http:\/\/www\.w3\.org\/TR\/xhtml11\/DTD\/xhtml11\.dtd">)\s*/;
 
 export function ejsPrecompile() {
     return {
         name: 'ejs-precompile',
         enforce: 'pre',
-        transform(src, id) {
-            if (!id.endsWith('.ejs')) return null;
+        transform(template, filename) {
+            if (!filename.endsWith('.ejs')) {
+                return null;
+            }
 
-            const compiled = lodashTemplate(src, {
+            if (
+                (filename.endsWith('.html.ejs')) ||
+                (filename.endsWith('.opf.ejs')) ||
+                (filename.endsWith('.ncx.ejs'))
+            ) {
+                template = minifyXML(template, {
+                    collapseEmptyElements: false,
+                    collapseWhitespaceInProlog: false,
+                    collapseWhitespaceInDocType: false,
+                    removeUnnecessaryStandaloneDeclaration: false,
+                    removeUnusedNamespaces: false,
+                    removeUnusedDefaultNamespace: false,
+                    shortenNamespaces: false,
+                });
+                template = template
+                    .replace(XML_PROLOG_REGEX, (_, expr) => `${expr}\n`)
+                    .replace(XHTML_DOCTYPE_REGEX, (_, expr) => `${expr}\n`);
+            }
+
+            const compiled = lodashTemplate(template, {
                 // Match EJS semantics: <%= %> is escaped, <%- %> is raw
                 escape: /<%=([\s\S]+?)%>/g,
                 interpolate: /<%-([^>]+?)%>/g,
